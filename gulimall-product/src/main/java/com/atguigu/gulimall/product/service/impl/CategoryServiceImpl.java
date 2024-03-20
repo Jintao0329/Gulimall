7bbcff564021a7f27012028846ec66fba37ffde4
Jintao0329 <jintaofu0329@gmail.com>
2024-03-20 16:48:21 +0100

    product三级分类

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.atguigu.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -25,5 +29,41 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
         return new PageUtils(page);
     }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1、查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        //2、组装成父子树形结构
+        //1)找到所有一级分类
+        List<CategoryEntity> level1Menus = entities.stream().filter((categoryEntity) -> {
+            return categoryEntity.getParentCid() == 0;
+        }).map((menu)->{
+            menu.setChildren(getChildren(menu,entities));
+            return  menu;
+        }).sorted((menu1,menu2)->{
+            return (menu1.getSort() == null?0:menu1.getSort()) - (menu2.getSort() == null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+
+
+
+
+        return level1Menus;
+    }
+    //递归查找所有菜单的子菜单
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all){
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid() == root.getCatId();
+        }).map(categoryEntity -> {
+            //1, 找到子菜单，递归
+            categoryEntity.setChildren(getChildren(categoryEntity,all));
+            return categoryEntity;
+        }).sorted((menu1,menu2) -> {
+            //2， 菜单的排序
+            return (menu1.getSort() == null?0:menu1.getSort()) - (menu2.getSort() == null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+        return children;
+    }
+
 
 }
