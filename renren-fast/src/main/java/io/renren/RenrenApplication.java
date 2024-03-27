f83077b8ebca2407b00f444d1b7827c88f6a4dd2
Jintao0329 <jintaofu0329@gmail.com>
2024-03-27 18:10:26 +0100

    renren-fast

--- renren-fast/src/main/java/io/renren/RenrenApplication.java
+++ renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -11,9 +11,12 @@ package io.renren;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
+
 public class RenrenApplication {
 
     public static void main(String[] args) {
